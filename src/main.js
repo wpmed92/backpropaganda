20d826df57f9d6c18865540401779946ea200aae
Ahmed Harmouche <ahmedharmouche92@gmail.com>
2019-06-11 10:23:07 +0200

    Refactor train into perceptron.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,4 @@
 var SingleLayerPerceptron = require("./single-layer-perceptron");
-var mathUtil = require("./util.js");
-let perceptron = new SingleLayerPerceptron(3);
 let trainData = [[1,0,0],
                  [1,1,1],
                  [0,1,0],
@@ -9,56 +7,10 @@ let trainData = [[1,0,0],
 
 let trainOutput = [1,1,0,0,0];
 let trainIterations = 1000;
-let deltas = createDeltasArray(3);
-let learnRate = 1;
+let learnRate = 10;
 
-function createDeltasArray(cols) {
-    let deltas = [];
-
-    for (let i = 0; i < cols; i++) {
-        deltas[i] = [];
-    }
-
-    return deltas;
-}
-
-//Train iterations
-for (let i = 0; i < trainIterations; i++) {
-    console.log("Train iteration " + i + "...");
-
-    //Learn from train data
-    for (let j = 0; j < trainData.length; j++) {
-        let input = trainData[j];
-        console.log("Input " + input.toString());
-        perceptron.loadInput(input);
-        let output = perceptron.activate();
-        let expectedOutput = trainOutput[j];
-        console.log("Expected output: " + expectedOutput, "Actual output: " + output);
-        let error = (output - expectedOutput) * (output - expectedOutput);
-        console.log("Error: " + error);
-
-        for (let k = 0; k < 3; k++) {
-            let gradient = 2*(output - expectedOutput)*mathUtil.sigmaDeriv(output)*input[k];
-            deltas[k][j] = gradient;
-        }
-    }
-
-    let avgDelta = [];
-
-    for (let i = 0; i < 3; i++) {
-        let avg = 0;
-
-        for (let j = 0; j < trainData.length; j++) {
-            avg += deltas[i][j];
-        }
-
-        avg /= trainData.length;
-        avgDelta[i] = avg;
-    }
-
-    console.log("Avarage delta " + avgDelta.toString());
-    perceptron.updateWeights(avgDelta, learnRate);
-}
+let perceptron = new SingleLayerPerceptron(3);
+perceptron.train(trainData, trainOutput, learnRate, trainIterations);
 
 console.log("Test train data.");
 
--- src/single-layer-perceptron.js
+++ src/single-layer-perceptron.js
@@ -1,16 +1,77 @@
+var mathUtil = require("./util.js");
+
 class SingleLayerPerceptron {
     constructor(neuronCount) {
         this.neuronCount = neuronCount;
         this.weights = [];
-        this.inputs = [];
+        this.input = [];
 
         for (let i = 0; i < neuronCount; i++) {
             this.weights.push(Math.random(0, 1));
         }
     }
 
-    loadInput(inputs) {
-        this.inputs = inputs;
+    createDeltasArray() {
+        let deltas = [];
+    
+        for (let i = 0; i < this.neuronCount; i++) {
+            deltas[i] = [];
+        }
+    
+        return deltas;
+    }
+
+    train(trainData, trainOutput, learnRate, trainIterations) {
+        let deltas = this.createDeltasArray();
+
+        for (let i = 0; i < trainIterations; i++) {
+            console.log("Train iteration " + i + "...");
+        
+            //Learn from train data
+            for (let j = 0; j < trainData.length; j++) {
+                let input = trainData[j];
+
+                //Load input
+                console.log("Input " + input.toString());
+                this.loadInput(input);
+
+                //Activate
+                let output = this.activate();
+
+                //Error calc
+                let expectedOutput = trainOutput[j];
+                console.log("Expected output: " + expectedOutput, "Actual output: " + output);
+                let error = (output - expectedOutput) * (output - expectedOutput);
+                console.log("Error: " + error);
+        
+                //Gradient calc
+                for (let k = 0; k < 3; k++) {
+                    let gradient = 2*(output - expectedOutput)*mathUtil.sigmaDeriv(output)*input[k];
+                    deltas[k][j] = gradient;
+                }
+            }
+        
+            //Avaraging gradients
+            let avgDelta = [];
+        
+            for (let i = 0; i < 3; i++) {
+                let avg = 0;
+        
+                for (let j = 0; j < trainData.length; j++) {
+                    avg += deltas[i][j];
+                }
+        
+                avg /= trainData.length;
+                avgDelta[i] = avg;
+            }
+        
+            console.log("Avarage delta " + avgDelta.toString());
+            this.updateWeights(avgDelta, learnRate);
+        }
+    }
+
+    loadInput(input) {
+        this.input = input;
     }
 
     updateWeights(deltas, lr) {
@@ -26,11 +87,11 @@ class SingleLayerPerceptron {
     activate() {
         let sum = 0;
 
-        for (let i = 0; i < this.inputs.length; i++) {
-            sum += this.inputs[i] * this.weights[i];    
+        for (let i = 0; i < this.input.length; i++) {
+            sum += this.input[i] * this.weights[i];    
         }
 
-        return 1/(1+(Math.pow(Math.E, -sum)));
+        return mathUtil.sigma(sum);
     }
 }
 
