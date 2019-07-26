96f09ed71ac0ab7c56d75e7c71509fa92a66a60e
Ahmed Harmouche <ahmedharmouche92@gmail.com>
2019-07-26 15:43:16 +0200

    WIP

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 #!/usr/bin/env node
 
+//cli
 const program = require('commander');
 const figlet = require('figlet');
 const { spawn, fork } = require('child_process')
 var path = require("path");
 var inquirer = require("inquirer");
+//nn
+var activations = require('./lib/activations');
 
 console.log(figlet.textSync('backpropaganda'));
 
-inquirer
+/*inquirer
   .prompt([
     {
       type: 'list',
@@ -49,21 +52,58 @@ inquirer
     } else {
       console.log("Not yet supported.");
     }
-  });
+  });*/
 
-/*program
-  .option('-e, --example <example>', 'Select an example')
+program
+  .option('-e, --example', 'Select an example')
+  .option('-a, --activation <activation>', 'Select an activation function')
   .action(function() {
-     if (program.example === "xor") {
-      fork(path.join(__dirname, "/examples/multi-layer-xor-train.js"))
-      .on('exit', code => {
-        console.log(`xor example run successfully`);
+     if (program.example) {
+      inquirer
+      .prompt([
+        {
+          type: 'list',
+          message: 'Select an example',
+          name: 'examples',
+          choices: [
+            {
+              name: 'Build a network',
+              value: "build"
+            },
+            {
+              name: 'The XOR problem',
+              value: "xor"
+            },
+            {
+              name: 'Recognizing handwritten digits',
+              value: "mnist"
+            },
+            {
+              name: 'Recognizing clothes',
+              value: "fashion"
+            }
+        ]}
+      ])
+      .then(answers => {
+        if (answers.examples === "build") {
+          console.log("Yay, build a network!");
+        } else if (answers.examples === "xor") {
+          fork(path.join(__dirname, "/examples/multi-layer-xor-train.js"))
+          .on('exit', code => {
+            console.log(`xor example run successfully`);
+          });
+        } else if (answers.examples === "mnist") {
+          fork(path.join(__dirname, "/examples/multi-layer-mnist-train.js"))
+          .on('exit', code => {
+            console.log(`mnist example run successfully`);
+          });
+        } else {
+          console.log("Not yet supported.");
+        }
       });
-     } else if (program.example === "mnist") {
-      fork(path.join(__dirname, "/examples/multi-layer-mnist-train.js"))
-      .on('exit', code => {
-        console.log(`mnist example run successfully`);
-      });
+     } else if (program.activation) {
+        var act = new activations[program.activation]();
+        act.print();
      }
   })
- .parse(process.argv);*/
+ .parse(process.argv);
